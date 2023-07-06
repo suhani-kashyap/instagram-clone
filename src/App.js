cd50c21a9ba3062ea630f0589844a353a746ff4f
Suhani Kashyap <suhani.kashyap@mail.mcgill.ca>
2023-07-06 17:21:07 -0400

    Added instagram logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
 
-    {/* Header */}
+   
+    <Header />
     
+    
+
     {/* Posts */}
     {/* Posts */}
     {/* Posts */}
---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className='header'>
+        <img 
+        className='header__img'
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png" 
+        alt="Instagram logo"/>
+    </div>
+  )
+}
+
+export default Header
