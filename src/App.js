d5267e3e637b2aef033f40634d4e9aab0005733d
Suhani Kashyap <suhani.kashyap@mail.mcgill.ca>
2023-07-06 17:27:05 -0400

    created post folder and sub files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Header/Header';
+import Post from './Post/Post';
 
 function App() {
   return (
     <div className="App">
-
-   
+    
     <Header />
-    
-    
-
-    {/* Posts */}
-    {/* Posts */}
-    {/* Posts */}
-      <h1>Instagram Clone!</h1>
+    <Post />
+  
     </div>
   );
 }
