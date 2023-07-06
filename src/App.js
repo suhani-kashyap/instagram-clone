5763138889e1070cd3ecddc300124ee34e09b7d9
Suhani Kashyap <suhani.kashyap@mail.mcgill.ca>
2023-07-06 14:41:12 -0400

    planned out app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+    {/* Header */}
+    
+    {/* Posts */}
+    {/* Posts */}
+    {/* Posts */}
       <h1>Instagram Clone!</h1>
     </div>
   );
